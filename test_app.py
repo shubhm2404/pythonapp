2aa66725f9876a0dd1005fe1e21048b76919676a
Shubham Dige <digeshubham24@gmail.com>
2026-08-16 14:49:01 +0000

    Adding Tests 1

--- test_app.py
+++ test_app.py
@@ -5,4 +5,4 @@ def test_home():
     response = client.get("/")
     
     assert response.status_code == 200
-    assert response.data == b"Hello, Jenkins!"
+    assert response.data == b"Hello! My CI/CD Pipeline is Working!"
